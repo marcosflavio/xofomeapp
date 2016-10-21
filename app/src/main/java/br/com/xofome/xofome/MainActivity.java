18ecc00e8078e032ac68b7f54dd195072131ca11
marcosf <marcosflaviosilveira@gmail.com>
2016-10-21 16:13:04 -0200

    Design CardView 0.3

--- app/src/main/java/br/com/xofome/xofome/MainActivity.java
+++ app/src/main/java/br/com/xofome/xofome/MainActivity.java
@@ -2,6 +2,12 @@ package br.com.xofome.xofome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
